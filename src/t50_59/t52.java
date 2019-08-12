40ea20779c19a3a28316b515cf9421c97f284b56
Nankis <1003863005@qq.com>
2019-08-12 22:36:51 +0800

    dayn  判断两个链表的第一个公共节点

---
+++ src/t50_59/t52.java
@@ -0,0 +1,28 @@
+package t50_59;
+
+/**
+ * 输入两个链表，找出它们的第一个公共结点。
+ */
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class t52 {
+}
+
+class Solution52 {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        ListNode l1 = pHead1, l2 = pHead2;
+        while (l1 != l2) {
+            l1 = (l1 == null) ? pHead2 : l1.next;
+            l2 = (l2 == null) ? pHead1 : l2.next;
+        }
+        return l1;
+    }
+}
